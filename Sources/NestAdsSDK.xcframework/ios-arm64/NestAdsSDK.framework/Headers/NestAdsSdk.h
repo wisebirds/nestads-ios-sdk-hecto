ea0c038a6c677a203841313be99af274369467b7
Bitbucket Pipeline <ci@wisebirds.com>
2025-12-09 21:24:42 +0900

    release v2.7.9 HECTO

---
+++ Sources/NestAdsSDK.xcframework/ios-arm64/NestAdsSDK.framework/Headers/NestAdsSdk.h
@@ -0,0 +1,18 @@
+//
+//  NestAdsSdk.h
+//  NestAdsSdk
+//
+//  Created by garam lee on 2023/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NestAdsSdk.
+FOUNDATION_EXPORT double NestAdsSdkVersionNumber;
+
+//! Project version string for NestAdsSdk.
+FOUNDATION_EXPORT const unsigned char NestAdsSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NestAdsSdk/PublicHeader.h>
+
+
